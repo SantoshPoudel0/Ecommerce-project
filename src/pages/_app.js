67620f21d5981b789f7af5a3872210d00320b884
Santosh Poudel <128451797+SantoshPoudel0@users.noreply.github.com>
2023-05-01 14:54:40 +0545

    adding some jsx code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <p>app Component  </p>
+    <Component user="Ram" {...pageProps} />
+  </>
 }
---
+++ src/pages/index.jsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+
+
+
+
+export default function Home(user) {
+  return (
+    <>
+
+      <header className= 'bg-primary text-white'>
+ 
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Product</li>
+        </ul>
+      </nav>
+    </header >
+    </>
+  )
+}
